e31bf79877e074e8b5e8dd62f1d85aad02d7c31b
rishanVJrathne <wijerathneprc92@gmail.com>
2023-01-03 20:03:45 +0530

    added login and logout features
    added new features of login and logout under acconts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def login_view(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username = username, password = password)
+
+        if user is None:
+            context = {"error": "Invalid username of password..."}
+            return render(request, "accounts/login.html", context)
+        
+        login(request, user)
+        return redirect('/')
+
+    return render(request, "accounts/login.html", {})
+
+
+def logout_view(request):
+    
+    if request.method == "POST":
+        logout(request)
+        return redirect("/accounts/login/")
+    return render(request, "accounts/logout.html", {})
+
+
+def register_view(request):
+    return render(request, "accounts/register.html", {})
