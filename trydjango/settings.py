0ed1dc04a3cc1e7e203ad6d8a0952af980658e49
rishanVJrathne <wijerathneprc92@gmail.com>
2023-01-05 10:29:20 +0530

    adding .env file

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d04dibh*3d4%-u&fgihh=1owaj^e5vuu8_e4ufa5nz&qn$-&&q'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-d04dibh*3d4%-u&fgihh=1owaj^e5vuu8_e4ufa5nz&qn$-&&q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
